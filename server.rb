b02d876c58e9a79838d6f0480d3b60aa146e8bc0
Nick McDonnough <nickmcd@gmail.com>
2014-09-10 21:51:51 -0500

    initial songify2 commit

--- server.rb
+++ server.rb
@@ -3,41 +3,55 @@ require 'sinatra/reloader'
 require 'pry-byebug'
 require_relative 'lib/songify.rb'
 
-get '/' do
-  redirect to '/index'
-end
+class Songify::Server < Sinatra::Application
 
-get '/index' do
-  erb :index, :locals => {title: 'Nick\'s Song Manager'}
-end
+  # if using vagrant uncomment the following 3 lines
+  # configure do
+  #   set :bind, '0.0.0.0'
+  # end
 
-get '/show' do
-  songs = Songify.songs.all
-  erb :show, :locals => {
-    title: 'All Songs | NSM',
-    songs: songs
-  }
-end
-
-get '/show/:id' do
-  song = Songify.songs.find(params[:id])
-  erb :song, :locals => {
-    title: 'Song: ' + song.title + ' | NSM',
-    song: song
-  }
-end
-
-get '/new' do
-  erb :new, :locals => {title: 'New Song | NSM'}
-end
-
-post '/create' do
-  song = Songify::Song.new(params['title'], params['artist'], params['album'])
-  Songify.songs.save(song)
-  redirect to '/show/' + song.id.to_s
-end
-
-get '/song/delete/:id' do
-  Songify.songs.delete(params[:id])
-  redirect to '/show'
+  # index
+  get '/' do
+    redirect to '/index'
+  end
+  
+  get '/index' do
+    erb :index, :locals => {title: 'Nick\'s Song Manager'}
+  end
+  
+  # show
+  get '/songs' do
+    songs = Songify.songs.all
+    erb :show, :locals => {
+      title: 'All Songs | NSM',
+      songs: songs
+    }
+  end
+  
+  # show
+  get '/songs/:id' do
+    song = Songify.songs.find(params[:id])
+    erb :song, :locals => {
+      title: 'Song: ' + song.title + ' | NSM',
+      song: song
+    }
+  end
+  
+  # new
+  get '/songs/new' do
+    erb :new, :locals => {title: 'New Song | NSM'}
+  end
+  
+  # create
+  post '/songs/create' do
+    song = Songify::Song.new(params['title'], params['artist'], params['album'])
+    Songify.songs.save(song)
+    redirect to '/songs/show/' + song.id.to_s
+  end
+  
+  # delete
+  get '/songs/delete/:id' do
+    Songify.songs.delete(params[:id])
+    redirect to '/songs/show'
+  end
 end
